5c84c9c04717d22966567fb3fde3c749c0d1873c
Carl S <sinocruc@oregonstate.edu>
2020-03-10 07:57:57 -0700

    random key generator is functioning; still need to implement creating a file containing the key

---
+++ keygen.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define CHAR_SET_SZ 27
+
+char* RandomKey(size_t key_length);
+void MakeKeyFile(char* otp_key);
+
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+    char* otp_key;
+    size_t key_length = atoi(argv[1]); //convert string value to int
+
+    otp_key = RandomKey(key_length);
+    //printf("otp_key: %s[%d]\n", otp_key, strlen(otp_key));
+    //MakeKeyFile(otp_key);    
+    
+    free(otp_key); //free memory allocated by RandomKey()
+    return 0;
+}
+/**********************************************************************
+ * 
+ **********************************************************************/
+char* RandomKey(size_t key_length){
+    //key generated must contain only the 26 upper-case letters of the alphabet and space char
+    static char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    int i, index;
+    char* rand_key;
+    //allocate memory for key; key_length + 1 to account for newline char
+    rand_key = malloc(sizeof(char) * key_length + 1); 
+    
+    //randomly fill rand_key with chars from char_set
+    for(i = 0; i < key_length; i++){
+        index = rand() % CHAR_SET_SZ; //randomly select an index from 0 to 26
+        rand_key[i] = char_set[index]; //add char to key
+    }
+    rand_key[key_length] = '\0';//add null terminator to end of string
+    return rand_key;
+}
+/**********************************************************************
+ * 
+ **********************************************************************/
+void MakeKeyFile(char* otp_key){
+
+}
