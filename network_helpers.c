93aa96d02e98f9578bbe844239d57d389c4f2df8
Carl S <sinocruc@oregonstate.edu>
2020-03-12 12:34:44 -0700

    VerifiedClient(), RecvMsg(), SendMsg() are all functioning but require additional testing. need to implement file sending and receiving methods next

--- network_helpers.c
+++ network_helpers.c
@@ -3,22 +3,21 @@
 /**********************************************************************
  * 
  **********************************************************************/
-bool VerifyConnection(int connection_fd, char* verification_msg){
-    char client_msg[32];
-    int chars_recvd = -5;
+bool VerifiedClient(int connection_fd, char* expected_id){
+    char client_id[32];
+    ssize_t chars_recvd = -5;
 
-    memset(client_msg, '\0', sizeof(client_msg)); //clear memory
-    chars_recvd = recv(connection_fd, client_msg, sizeof(client_msg) - 1, 0);
+    memset(client_id, '\0', sizeof(client_id)); //clear memory
+    chars_recvd = recv(connection_fd, client_id, sizeof(client_id) - 1, 0);
     
-    if(strcmp(verification_msg, client_msg) == 0){
-        char* message = "Connection successful";
+    if(strcmp(expected_id, client_id) == 0){
         return true;
     }
     else if(chars_recvd < 0){
-        perror("Error verifying connection: ");
+        perror("Error receiving client ID: ");
     }
     else if(chars_recvd == 0){
-        fprintf(stderr, "Unable to recv: Client closed connection\n");
+        fprintf(stderr, "Error receiving client ID: Client closed connection\n");
     }
     else{
         fprintf(stderr, "Verification does not match: Rejecting connection\n");
@@ -29,13 +28,46 @@ bool VerifyConnection(int connection_fd, char* verification_msg){
 /**********************************************************************
  * 
  **********************************************************************/
-void SendMsg(int connection_fd, char* message){
+bool RecvMsg(int connection_fd, char* expected_msg){
+    char client_msg[32];
+    ssize_t chars_recvd = -5;
 
+    memset(client_msg, '\0', sizeof(client_msg)); //clear memory
+    chars_recvd = recv(connection_fd, client_msg, sizeof(client_msg) - 1, 0);
+    
+    if(strcmp(expected_msg, client_msg) == 0){
+        return true;
+    }
+    else if(chars_recvd < 0){
+        perror("Error receiving message: ");
+    }
+    else if(chars_recvd == 0){
+        fprintf(stderr, "Error receiving message: Connection closed\n");
+    }
+    close(connection_fd);
+    return false;
 }
 /**********************************************************************
  * 
  **********************************************************************/
+void SendMsg(int connection_fd, char* message){
+    ssize_t chars_sent;
 
+    chars_sent = send(connection_fd, message, strlen(message), 0);
+    if(chars_sent == -1){
+        perror("Error sending message: ");
+        close(connection_fd);
+    }
+}
 /**********************************************************************
  * 
  **********************************************************************/
+//char* RecvFileText(int connection_fd){
+//    
+//}
+/**********************************************************************
+ * 
+ **********************************************************************/
+//void SendFileText(int connection_fd, char* file_name){
+//    
+//}
--- network_helpers.h
+++ network_helpers.h
@@ -5,10 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
+bool VerifiedClient(int connection_fd, char* client_id);
 bool RecvMsg(int connection_fd, char* expected_msg);
 void SendMsg(int connection_fd, char* message);
-char* RecvFileText(int connection_fd);
-void SendFileText(int connection_fd, char* file_name);
+//char* RecvFileText(int connection_fd);
+//void SendFileText(int connection_fd, char* file_name);
 
 #endif
