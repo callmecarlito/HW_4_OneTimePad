a618f8838d44dece5b5afc4faf1c68df0bbc863e
Carl S <sinocruc@oregonstate.edu>
2020-03-11 21:27:29 -0700

    minor changes to error messages and spacing

--- keygen.c
+++ keygen.c
@@ -13,15 +13,21 @@ int main(int argc, char *argv[]){
     char* otp_key;
     size_t key_length = atoi(argv[1]); //convert string value to int
 
-    otp_key = RandomKey(key_length);
+    //checks that atoi was successful in converting to int
+    if(key_length == 0){
+        fprintf(stderr, "keygen error: Invalid length entered\n");
+        exit(1);
+    }
+
+    otp_key = RandomKey(key_length); //generate the random key
     //printf("otp_key: %s[%d]\n", otp_key, strlen(otp_key));
-    WriteKey(otp_key);    
+    WriteKey(otp_key); //write otp key to file    
     
     free(otp_key); //free memory allocated by RandomKey()
     return 0;
 }
 /**********************************************************************
- * 
+ * https://codereview.stackexchange.com/questions/29198/random-string-generator-in-c
  **********************************************************************/
 char* RandomKey(size_t key_length){
     //key generated must contain only the 26 upper-case letters of the alphabet and space char
@@ -44,14 +50,15 @@ char* RandomKey(size_t key_length){
  * 
  **********************************************************************/
 void WriteKey(char* otp_key){
-    FILE* output_file = fopen("key_file", "w+");
+    //open file for writing
+    FILE* output_file = fopen("key_file", "w+"); //create if file doesn't exist and overwrite existing file
+    //if fopen() was unsuccessful
     if(output_file == NULL){
-        perror("Unable to create output file: ");
+        perror("keygen error: Unable to create output file: ");
         exit(1);
     }
     else{
         fprintf(output_file, "%s\n", otp_key);
         fclose(output_file);
     }
-
 }
