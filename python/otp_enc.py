5df52da9ddc82367cd77dde5d917ba6486c64acd
Carl S <sinocruc@oregonstate.edu>
2021-01-21 21:40:57 -0800

    working basic socket

--- python/otp_enc.py
+++ python/otp_enc.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 10
+
 HOST = socket.gethostname()
 PORT = 5757
 
@@ -7,5 +9,20 @@ PORT = 5757
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect((HOST, PORT))
 
-msg = s.recv(1024) #receives 1024 bytes of data at a time
-print(msg.decode("utf-8"))
+while True:
+        
+    full_msg =''
+    new_msg = True
+    while True:
+        msg = s.recv(16) #receives 16 bytes of data at a time
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msg_len = int(msg[:HEADERSIZE])
+            new_msg = False
+        full_msg += msg.decode("utf-8")
+        if len(full_msg)-HEADERSIZE == msg_len:
+            print("Full message received.")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+    print(full_msg)
--- python/otp_enc_d.py
+++ python/otp_enc_d.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import socket
 
+HEADERSIZE = 10
 #HOST = '127.0.0.1'  #standard loopback interface address (localhost)
 HOST = socket.gethostname()
 PORT = 5757        #Port to listen on 
@@ -13,5 +14,13 @@ s.listen(5) #establishes listening socket - limited to 5 connect requests
 while True:
     clientsocket, address = s.accept() #accept incoming connections
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    
+    msg = "Welcome to the server!"
+    # :<10 is the header length, left aligned(<)
+    print(f'{len(msg):<{HEADERSIZE}}' + msg)
+    # curly brackets allow us to embed a varialbe within another variable
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+     
+    clientsocket.send(bytes(msg, "utf-8"))
+    
 
