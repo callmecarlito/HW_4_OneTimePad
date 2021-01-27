340ae78a356be03f5e2d822d1310ab9735a7f203
Carl S <sinocruc@oregonstate.edu>
2021-01-26 21:58:38 -0800

    added some socket error handling

--- python/otp_enc.py
+++ python/otp_enc.py
@@ -1,28 +1,53 @@
+#!/usr/bin/env python3
 import socket
+import sys
 
 HEADERSIZE = 10
 
 HOST = socket.gethostname()
 PORT = 5757
 
-#create socket object using internet address family IPv4, SOCK_STREAM = TCP socket type
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((HOST, PORT))
+s = None
+#set up address for server
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_INET, socket.SOCK_STREAM):
+    af, socketype, proto, canonname, servaddr = res
+    try:
+        #create socket object using internet address family IPv4, SOCK_STREAM = TCP socket type
+        s = socket.socket(af, socketype, proto)
+    except OSError as msg:
+        s = None
+        continue
+    try: 
+        #connect to server
+        s.connect(servaddr)
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
 
-while True:
-        
-    full_msg =''
-    new_msg = True
+if s is None:
+    print("could not open socket")
+    sys.exit(1)
+
+with s:
     while True:
-        msg = s.recv(16) #receives 16 bytes of data at a time
-        if new_msg:
-            print(f"new message length: {msg[:HEADERSIZE]}")
-            msg_len = int(msg[:HEADERSIZE])
-            new_msg = False
-        full_msg += msg.decode("utf-8")
-        if len(full_msg)-HEADERSIZE == msg_len:
-            print("Full message received.")
-            print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ''
-    print(full_msg)
+            
+        full_msg = ''
+        new_msg = True
+
+        while True:
+            msg = s.recv(16) #receives 16 bytes of data at a time
+            
+            if new_msg:
+                print(f"new message length: {msg[:HEADERSIZE]}")
+                msg_len = int(msg[:HEADERSIZE])
+                new_msg = False
+
+            full_msg += msg.decode("utf-8")
+
+            if (len(full_msg) - HEADERSIZE) == msg_len:
+                print("Full message received.")
+                print(full_msg[HEADERSIZE:])
+                new_msg = True
+                full_msg = ''
--- python/otp_enc_d.py
+++ python/otp_enc_d.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python3
 import socket
+import sys
+import time
 
 HEADERSIZE = 10
+
 #HOST = '127.0.0.1'  #standard loopback interface address (localhost)
 HOST = socket.gethostname()
 PORT = 5757        #Port to listen on 
 
-#create socket object using internet address family IPv4, SOCK_STREAM = TCP socket type
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((HOST, PORT)) #binds socket to port
-s.listen(5) #establishes listening socket - limited to 5 connect requests
+s = None
+#set up address for server
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_INET, socket.SOCK_STREAM, 
+                              0, socket.AI_PASSIVE):
+    af, socktype, proto, canonname, servaddr = res
+    try:
+        #create socket object using internet address family IPv4, SOCK_STREAM = TCP socket type
+        s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        s = None
+        continue
+    try: 
+        s.bind(servaddr) #binds socket to address
+        s.listen(5) #establishes listening socket - limited to 5 connect requests
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print("unable to open socket")
+    sys.exit(1)
 
 while True:
-    clientsocket, address = s.accept() #accept incoming connections
-    print(f"Connection from {address} has been established!")
-    
-    msg = "Welcome to the server!"
-    # :<10 is the header length, left aligned(<)
-    print(f'{len(msg):<{HEADERSIZE}}' + msg)
-    # curly brackets allow us to embed a varialbe within another variable
-    msg = f'{len(msg):<{HEADERSIZE}}' + msg
-     
-    clientsocket.send(bytes(msg, "utf-8"))
-    
-
+    clientsocket, clientaddr = s.accept() #accept incoming connections
+    with clientsocket:
+        print(f"connection from {clientaddr} has been established")
+        
+        msg = "Welcome to the server!"
+        # :<10 is the header length, left aligned(<)
+        print(f'{len(msg):<{HEADERSIZE}}' + msg)
+        # curly brackets allow us to embed a varialbe within another variable
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        
+        clientsocket.send(bytes(msg, "utf-8"))
+        
+        while True:
+            time.sleep(3)
+            msg = f'The time is: {time.time()}'
+            msg = f'{len(msg):<{HEADERSIZE}}' + msg
+            clientsocket.send(bytes(msg, "utf-8"))
