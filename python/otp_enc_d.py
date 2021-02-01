b2354705953c406695d364e7655813aa1f8380ba
Carl S <sinocruc@oregonstate.edu>
2021-02-01 08:18:05 -0800

    implemented a ServerSocket class

--- python/otp_enc_d.py
+++ python/otp_enc_d.py
@@ -5,49 +5,70 @@ import time
 
 HEADERSIZE = 10
 
-#HOST = '127.0.0.1'  #standard loopback interface address (localhost)
-HOST = socket.gethostname()
-PORT = 5757        #Port to listen on 
+class SocketServer():
 
-s = None
-#set up address for server
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_INET, socket.SOCK_STREAM, 
-                              0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, servaddr = res
-    try:
-        #create socket object using internet address family IPv4, SOCK_STREAM = TCP socket type
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try: 
-        s.bind(servaddr) #binds socket to address
-        s.listen(5) #establishes listening socket - limited to 5 connect requests
-    except OSError as msg:
-        s.close()
-        s = None
-        continue
-    break
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = None
+    
+    def start_server(self):
 
-if s is None:
-    print("unable to open socket")
-    sys.exit(1)
+        try:
+            #create AF_INET(IPv4), TCP socket stream
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Server socket created")
+        except OSError as e:
+            print(f"Unable to create socket\n Error: {e}")
+            self.server_socket = None
+        
+        try:
+            self.server_socket.bind((self.host, self.port)) #bind socket to address
+            print("Binding server socket")
+            self.server_socket.listen(5)  #establishes listening socket - limited to 5 connect requests
+            print("Listening for incoming connections")
+        except OSError as e:
+            print(f"Unable to bind socket\n Error: {e[0]}, {e[1]}")
+            self.server_socket = None
+        
+        if s is None:
+            print("Unable to establish server socket")
+            sys.exit(1)
 
-while True:
-    clientsocket, clientaddr = s.accept() #accept incoming connections
-    with clientsocket:
-        print(f"connection from {clientaddr} has been established")
-        
-        msg = "Welcome to the server!"
-        # :<10 is the header length, left aligned(<)
-        print(f'{len(msg):<{HEADERSIZE}}' + msg)
-        # curly brackets allow us to embed a varialbe within another variable
-        msg = f'{len(msg):<{HEADERSIZE}}' + msg
-        
-        clientsocket.send(bytes(msg, "utf-8"))
-        
         while True:
-            time.sleep(3)
-            msg = f'The time is: {time.time()}'
-            msg = f'{len(msg):<{HEADERSIZE}}' + msg
-            clientsocket.send(bytes(msg, "utf-8"))
+            client_socket, clientaddr = self.server_socket.accept() #accept incoming connections
+            with client_socket:
+                print(f"Connected to: {clientaddr}")
+                
+                msg = "Welcome to the server!"
+                # :<10 is the header length, left aligned(<)
+                print(f'{len(msg):<{HEADERSIZE}}' + msg)
+                # curly brackets allow us to embed a varialbe within another variable
+                msg = f'{len(msg):<{HEADERSIZE}}' + msg
+                
+                client_socket.send(bytes(msg, "utf-8"))
+                
+                while True:
+                    time.sleep(3)
+                    msg = f'The time is: {time.time()}'
+                    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+                    client_socket.send(bytes(msg, "utf-8"))        
+        
+#should use command line arg for port number
+    #don't forget to cast as int
+#should run in background or as a daemon
+#should output error if there's a network error
+
+#after connection is made:
+    #verify client is otp_enc
+    #then receive plaintext file and key from otp_enc
+    #verify key passed should be equal in size to the plaintext
+
+#server encrypts the plaintext and returns the ciphertext
+
+if __name__ == "__main__":
+    HOST = socket.gethostname()
+    PORT = 5757  
+    
+    s = SocketServer(HOST, PORT)
+    s.start_server()
